0097a7398885084d442c85e44f6a13b25f4bca20
Max Poletaev <max.poletaev@gmail.com>
2022-01-04 17:41:53 +0300

    More tests, package rename

--- bus.go
+++ bus.go
@@ -1,4 +1,4 @@
-package gobus
+package van
 
 import (
 	"context"
@@ -7,47 +7,45 @@ import (
 	"sync"
 )
 
-type provider interface{} // func(deps ...interface{}) interface{}
-type handler interface{}  // func(ctx context.Context, cmd interface{}, deps ...interface{}) error
-
-type emptyHandler func(ctx context.Context, cmd interface{}) error
+type providerFunc interface{} // func(deps ...interface{}) interface{}
+type handlerFunc interface{}  // func(ctx context.Context, cmd interface{}, deps ...interface{}) error
 
 type Bus interface {
-	Provide(providerFn interface{})
-	Handle(cmd interface{}, handlerFunc handler)
-	Invoke(ctx context.Context, cmd interface{}) error
-	Subscribe(event interface{}, listeners ...handler)
-	Emit(ctx context.Context, event interface{}) (chan struct{}, chan error)
+	Provide(provider providerFunc)
+	HandleCommand(cmd interface{}, handler handlerFunc)
+	InvokeCommand(ctx context.Context, cmd interface{}) error
+	ListenEvent(event interface{}, listeners ...handlerFunc)
+	EmitEvent(ctx context.Context, event interface{}) (chan struct{}, chan error)
 	Exec(fn interface{}) error
 }
 
 type busImpl struct {
-	providers map[reflect.Type]provider
-	handlers  map[reflect.Type]handler
-	listeners map[reflect.Type][]handler
+	providers map[reflect.Type]providerFunc
+	handlers  map[reflect.Type]handlerFunc
+	listeners map[reflect.Type][]handlerFunc
 }
 
 func New() Bus {
-	bus := &busImpl{}
-	bus.providers = make(map[reflect.Type]provider)
+	b := &busImpl{}
+	b.providers = make(map[reflect.Type]providerFunc)
 
-	// register artificial provider for Bus type so that we can access it from handlers
-	bus.providers[reflect.TypeOf((*Bus)(nil)).Elem()] = func() Bus {
-		return bus
+	// register provider for Bus type so that we can access it from handlers
+	b.providers[reflect.TypeOf((*Bus)(nil)).Elem()] = func() Bus {
+		return b
 	}
 
-	bus.handlers = make(map[reflect.Type]handler)
-	bus.listeners = make(map[reflect.Type][]handler)
-	return bus
+	b.handlers = make(map[reflect.Type]handlerFunc)
+	b.listeners = make(map[reflect.Type][]handlerFunc)
+	return b
 }
 
-func (b *busImpl) Provide(providerFn interface{}) {
-	providerType := reflect.TypeOf(providerFn)
+func (b *busImpl) Provide(provider providerFunc) {
+	providerType := reflect.TypeOf(provider)
 	switch {
 	case providerType.Kind() != reflect.Func:
 		panic("provider must be a function")
 	case providerType.NumOut() != 1:
-		panic("provider must have one retunrn value")
+		panic("provider must have one return value")
 	}
 
 	retType := providerType.Out(0)
@@ -66,60 +64,136 @@ func (b *busImpl) Provide(providerFn interface{}) {
 		}
 	}
 
-	b.providers[retType] = providerFn
+	b.providers[retType] = provider
 }
 
-func (b *busImpl) Handle(msg interface{}, handlerFunc handler) {
-	msgType := reflect.TypeOf(msg)
+func (b *busImpl) HandleCommand(cmd interface{}, handler handlerFunc) {
+	cmdType := reflect.TypeOf(cmd)
 	switch {
-	case msgType.Kind() != reflect.Struct:
+	case cmdType.Kind() != reflect.Struct:
 		panic("msg must be a struct")
 	}
 
-	if _, ok := b.handlers[msgType]; ok {
-		panic("handler already registered for type: " + msgType.String())
+	if _, ok := b.handlers[cmdType]; ok {
+		panic("handler already registered for type " + cmdType.String())
 	}
 
-	handlerT := reflect.TypeOf(handlerFunc)
-	switch {
-	case handlerT.Kind() != reflect.Func:
-		panic("handler must be a function")
-	case handlerT.NumIn() < 2:
-		panic("handler must have at least three arguments")
-	case !isTypeContext(handlerT.In(0)):
-		panic("handler's first argument must be a context")
-	case !isTypeStructPointer(handlerT.In(1)):
-		panic("handler's second argument must be a pointer to a struct")
-	}
+	handlerType := reflect.TypeOf(handler)
+	b.validateHandler(handlerType)
 
-	for i := 2; i < handlerT.NumIn(); i++ {
-		argType := handlerT.In(i)
+	for i := 2; i < handlerType.NumIn(); i++ {
+		argType := handlerType.In(i)
 		if argType.Kind() != reflect.Interface {
 			panic(fmt.Sprintf("handler's argument %d must be an interface", i))
 		}
 		if _, ok := b.providers[argType]; !ok {
-			panic("there's no provider found for type: " + argType.String())
+			panic("no providers registered for type " + argType.String())
 		}
 	}
 
-	if _, ok := b.handlers[msgType]; !ok {
-		b.handlers[msgType] = make([]handler, 0)
-	}
-
-	b.handlers[msgType] = handlerFunc
+	b.handlers[cmdType] = handler
 }
 
-func (b *busImpl) Subscribe(event interface{}, handlerFuncs ...handler) {
-	eventT := reflect.TypeOf(event)
-	if eventT.Kind() != reflect.Struct {
+func (b *busImpl) InvokeCommand(ctx context.Context, cmd interface{}) error {
+	cmdType := reflect.TypeOf(cmd)
+	if cmdType.Kind() != reflect.Ptr {
+		return fmt.Errorf("cmd must be a pointer to a struct")
+	}
+	cmdType = cmdType.Elem()
+	if cmdType.Kind() != reflect.Struct {
+		return fmt.Errorf("cmd must be a pointer to a struct")
+	}
+
+	handler, ok := b.handlers[cmdType]
+	if !ok {
+		return fmt.Errorf("no handlers found for type %s", cmdType.String())
+	}
+
+	handlerValue := reflect.ValueOf(handler)
+	args := make([]reflect.Value, handlerValue.Type().NumIn())
+	args[0] = reflect.ValueOf(ctx)
+	args[1] = reflect.ValueOf(cmd)
+
+	err := b.resolve(handlerValue, args)
+	if err != nil {
+		return err
+	}
+
+	ret := handlerValue.Call(args)
+	return toError(ret[0])
+}
+
+func (b *busImpl) ListenEvent(event interface{}, listeners ...handlerFunc) {
+	eventType := reflect.TypeOf(event)
+	if eventType.Kind() != reflect.Struct {
 		panic("event must be a struct")
 	}
 
-	if _, ok := b.listeners[eventT]; !ok {
-		b.listeners[eventT] = make([]handler, 0)
+	for i := range listeners {
+		listenerType := reflect.TypeOf(listeners[i])
+		b.validateListener(listenerType)
+		if eventType != listenerType.In(1) {
+			panic("event type mismatch")
+		}
 	}
 
-	b.listeners[eventT] = append(b.listeners[eventT], handlerFuncs...)
+	if _, ok := b.listeners[eventType]; !ok {
+		b.listeners[eventType] = make([]handlerFunc, 0)
+	}
+
+	b.listeners[eventType] = append(b.listeners[eventType], listeners...)
+}
+
+func (b *busImpl) EmitEvent(ctx context.Context, event interface{}) (done chan struct{}, errchan chan error) {
+	done = make(chan struct{})
+	errchan = make(chan error, 1)
+
+	eventType := reflect.TypeOf(event)
+	if eventType.Kind() != reflect.Struct {
+		errchan <- fmt.Errorf("event must be a a struct")
+		close(done)
+		close(errchan)
+		return
+	}
+
+	listeners, ok := b.listeners[eventType]
+	if !ok {
+		close(done)
+		close(errchan)
+		return
+	}
+
+	wg := &sync.WaitGroup{}
+	errchan = make(chan error, len(listeners))
+	for _, listener := range listeners {
+		listenerValue := reflect.ValueOf(listener)
+		args := make([]reflect.Value, listenerValue.Type().NumIn())
+		args[0] = reflect.ValueOf(ctx)
+		args[1] = reflect.ValueOf(event)
+
+		err := b.resolve(listenerValue, args)
+		if err != nil {
+			errchan <- err
+			continue
+		}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ret := listenerValue.Call(args)
+			if err := toError(ret[0]); err != nil {
+				errchan <- err
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(done)
+		close(errchan)
+	}()
+
+	return
 }
 
 func (b *busImpl) Exec(fn interface{}) error {
@@ -131,113 +205,22 @@ func (b *busImpl) Exec(fn interface{}) error {
 		return fmt.Errorf("return value must be an error")
 	}
 
-	numIn := funcType.NumIn()
-	if numIn == 0 {
-		// Avoid expensive reflect.Value.Call() if handler has no dependencies
-		return fn.(func() error)()
+	for i := 0; i <= funcType.NumIn(); i++ {
+		argType := funcType.In(i)
+		if argType.Kind() != reflect.Interface {
+			return fmt.Errorf("function argument %d is not an interface", i)
+		}
 	}
 
-	args := make([]reflect.Value, numIn)
+	args := make([]reflect.Value, funcType.NumIn())
 	funcValue := reflect.ValueOf(fn)
 	b.resolve(funcValue, args)
 	ret := funcValue.Call(args)
 	return toError(ret[0])
 }
 
-func (b *busImpl) Invoke(ctx context.Context, cmd interface{}) error {
-	cmdT := reflect.TypeOf(cmd)
-	if cmdT.Kind() != reflect.Ptr {
-		return fmt.Errorf("cmd must be a pointer to a struct")
-	}
-
-	cmdT = cmdT.Elem()
-	if cmdT.Kind() != reflect.Struct {
-		return fmt.Errorf("cmd must be a pointer to a struct")
-	}
-
-	handler, ok := b.handlers[cmdT]
-	if !ok {
-		return fmt.Errorf("no handlers found for type: %s", cmdT.String())
-	}
-
-	handlerV := reflect.ValueOf(handler)
-	numIn := handlerV.Type().NumIn()
-	if numIn == 2 {
-		// Avoid expensive reflect.Value.Call() if handler has no dependencies
-		return handler.(emptyHandler)(ctx, cmd)
-	}
-
-	args := make([]reflect.Value, numIn)
-	args[0] = reflect.ValueOf(ctx)
-	args[1] = reflect.ValueOf(cmd)
-
-	err := b.resolve(handlerV, args)
-	if err != nil {
-		return err
-	}
-
-	ret := handlerV.Call(args)
-	return toError(ret[0])
-}
-
-func (b *busImpl) Emit(ctx context.Context, event interface{}) (done chan struct{}, errchan chan error) {
-	eventT := reflect.TypeOf(event)
-
-	done = make(chan struct{})
-	errchan = make(chan error, 1)
-	if eventT.Kind() != reflect.Ptr {
-		errchan <- fmt.Errorf("event must be a pointer to a struct")
-		close(done)
-		return
-	}
-
-	eventT = eventT.Elem()
-	if eventT.Kind() != reflect.Struct {
-		errchan <- fmt.Errorf("event must be a pointer to a struct")
-		close(done)
-		return
-	}
-
-	listeners, ok := b.listeners[eventT]
-	if !ok {
-		close(done)
-		return
-	}
-
-	wg := sync.WaitGroup{}
-	errchan = make(chan error, len(listeners))
-	for _, listener := range listeners {
-		listenerV := reflect.ValueOf(listener)
-		args := make([]reflect.Value, listenerV.Type().NumIn())
-		args[0] = reflect.ValueOf(ctx)
-		args[1] = reflect.ValueOf(event)
-
-		err := b.resolve(listenerV, args)
-		if err != nil {
-			errchan <- err
-			continue
-		}
-
-		wg.Add(1)
-		go func(f reflect.Value, args []reflect.Value) {
-			defer wg.Done()
-			ret := f.Call(args)
-			if err := toError(ret[0]); err != nil {
-				errchan <- err
-			}
-		}(listenerV, args)
-	}
-
-	go func() {
-		wg.Wait()
-		close(done)
-	}()
-
-	return
-}
-
-func (b *busImpl) resolve(funcV reflect.Value, args []reflect.Value) error {
-	funcT := funcV.Type()
+func (b *busImpl) resolve(funcValue reflect.Value, args []reflect.Value) error {
+	funcT := funcValue.Type()
 	for i := 0; i < funcT.NumIn(); i++ {
 		if args[i].IsValid() {
 			if _, ok := args[i].Interface().(context.Context); ok {
@@ -250,17 +233,67 @@ func (b *busImpl) resolve(funcV reflect.Value, args []reflect.Value) error {
 			continue
 		}
 
-		providerFunc := b.providers[argType]
-		providerV := reflect.ValueOf(providerFunc)
-		nextArgs := make([]reflect.Value, providerV.Type().NumIn())
-		err := b.resolve(providerV, nextArgs)
+		provider := b.providers[argType]
+		providerType := reflect.TypeOf(provider)
+		providerValue := reflect.ValueOf(provider)
+
+		nextArgs := make([]reflect.Value, providerType.NumIn())
+		err := b.resolve(providerValue, nextArgs)
 		if err != nil {
 			return err
 		}
 
-		args[i] = providerV.Call(nextArgs)[0]
+		args[i] = providerValue.Call(nextArgs)[0]
 	}
 	return nil
+}
+
+func (b *busImpl) validateHandler(handlerType reflect.Type) {
+	switch {
+	case handlerType.Kind() != reflect.Func:
+		panic("handler must be a function")
+	case handlerType.NumIn() < 2:
+		panic("handler must have at least 2 arguments")
+	case !isTypeContext(handlerType.In(0)):
+		panic("handler's first argument must be the context")
+	case !isTypeStructPointer(handlerType.In(1)):
+		panic("handler's second argument must be a struct pointer")
+	}
+
+	// start from the third argument as the first two are always `ctx` and `cmd`
+	for i := 2; i < handlerType.NumIn(); i++ {
+		argType := handlerType.In(i)
+		if argType.Kind() != reflect.Interface {
+			panic(fmt.Sprintf("handler's argument %d must be an interface", i))
+		}
+		if _, ok := b.providers[argType]; !ok {
+			panic("no providers registered for type " + argType.String())
+		}
+	}
+}
+
+func (b *busImpl) validateListener(listenerType reflect.Type) {
+	switch {
+	case listenerType.Kind() != reflect.Func:
+		panic("listener must be a function")
+	case listenerType.NumIn() < 2:
+		panic("listener must have at least 2 arguments")
+	case !isTypeContext(listenerType.In(0)):
+		panic("listener's first argument must be the context")
+	case listenerType.In(1).Kind() != reflect.Struct:
+		panic("listener's second argument must be a struct")
+	}
+
+	// start from the third argument as the first two are always `ctx` and `event`
+	for i := 2; i < listenerType.NumIn(); i++ {
+		argType := listenerType.In(i)
+		if argType.Kind() != reflect.Interface {
+			panic(fmt.Sprintf("listener's argument %d must be an interface", i))
+		}
+		if _, ok := b.providers[argType]; !ok {
+			panic("no providers registered for type " + argType.String())
+		}
+	}
 }
 
 func isTypeStructPointer(t reflect.Type) bool {
--- bus_test.go
+++ bus_test.go
@@ -1,126 +1,359 @@
-package gobus
+package van
 
 import (
 	"context"
+	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-type service interface {
-	Get() int
+type Command struct {
+	Value int
+}
+type Event struct {
+	Value int
 }
 
-type serviceImpl struct{}
+type GetIntService interface {
+	Get() int
+}
+type GetIntServiceImpl struct{}
 
-func (s *serviceImpl) Get() int {
+func (s *GetIntServiceImpl) Get() int {
 	return 1
 }
 
-func assertPanic(t *testing.T, p interface{}, message string) {
-	v, ok := p.(string)
-	if !ok {
-		t.Errorf("expected panic with message '%s', got '%v'", message, p)
-		return
-	}
-	if v != message {
-		t.Errorf("expected panic with message '%s', got '%v'", message, v)
-	}
+type SetIntService interface {
+	Set(int)
+}
+type SetIntSevriceImpl struct{}
+
+func (s *SetIntSevriceImpl) Set(i int) {}
+
+type UnknownService interface{}
+
+func TestProvide(t *testing.T) {
+	bus := New().(*busImpl)
+	bus.Provide(func() GetIntService {
+		return &GetIntServiceImpl{}
+	})
+	assert.Len(t, bus.providers, 2)
 }
 
-func TestBus_Provide(t *testing.T) {
+func TestProvide_WithDeps(t *testing.T) {
 	bus := New().(*busImpl)
-	bus.Provide(func() service {
-		return &serviceImpl{}
+
+	setIntService := &SetIntSevriceImpl{}
+	bus.Provide(func() SetIntService {
+		return setIntService
 	})
-	if len(bus.providers) != 2 {
-		t.Errorf("expected 2 providers, got %d", len(bus.providers))
-	}
+
+	bus.Provide(func(s SetIntService) GetIntService {
+		assert.Equal(t, setIntService, s)
+		return &GetIntServiceImpl{}
+	})
+
+	assert.Len(t, bus.providers, 3)
 }
 
 func TestBus_ProvideFails(t *testing.T) {
 	tests := map[string]struct {
-		msg           interface{}
-		handler       interface{}
-		expectedPanic string
+		provider  interface{}
+		wantPanic string
 	}{
-		"msg not a struct": {
-			msg:           1,
-			expectedPanic: "msg must be a struct",
+		"not a func": {
+			provider:  1,
+			wantPanic: "provider must be a function",
 		},
 		"no return value": {
-			msg:           struct{}{},
-			handler:       func() {},
-			expectedPanic: "handler must have one retunrn value",
+			provider:  func() {},
+			wantPanic: "provider must have one return value",
+		},
+		"multiple return values": {
+			provider:  func() (int, int) { return 1, 2 },
+			wantPanic: "provider must have one return value",
 		},
 		"return value not an interface": {
-			msg:           struct{}{},
-			handler:       func() int { return 1 },
-			expectedPanic: "handler's return value must be an interface",
+			provider:  func() int { return 1 },
+			wantPanic: "provider's return value must be an interface",
+		},
+		"arg is not an interface": {
+			provider: func(int) GetIntService {
+				return &GetIntServiceImpl{}
+			},
+			wantPanic: "provider's argument 0 must be an interface",
+		},
+		"unknown interface": {
+			provider: func(s SetIntService) GetIntService {
+				return &GetIntServiceImpl{}
+			},
+			wantPanic: "no providers registered for type van.SetIntService",
 		},
 	}
+
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			defer func() {
-				if r := recover(); r == nil {
-					assertPanic(t, r, tt.expectedPanic)
-				}
-			}()
 			bus := New()
-			bus.Handle(tt.msg, tt.handler)
+			assert.PanicsWithValue(t, tt.wantPanic, func() {
+				bus.Provide(tt.provider)
+			})
 		})
 	}
 }
 
-func TestBus_Handle(t *testing.T) {
+func TesHandleCommand(t *testing.T) {
 	bus := New().(*busImpl)
 	type cmd struct{}
-	bus.Handle(cmd{}, func(ctx context.Context, cmd *cmd) error {
+	bus.HandleCommand(cmd{}, func(ctx context.Context, cmd *cmd) error {
 		return nil
 	})
-	if len(bus.handlers) != 1 {
-		t.Errorf("expected 1 handler, got %d", len(bus.handlers))
-	}
+	assert.Len(t, bus.handlers, 1)
 }
 
-func TestBus_HandleFails(t *testing.T) {
-	type args struct {
-		msg     interface{}
-		handler interface{}
-	}
+func TesHandleCommandFails(t *testing.T) {
 	tests := map[string]struct {
-		args      args
+		cmd       interface{}
+		handler   interface{}
 		wantPanic string
 	}{
 		"msg not a struct": {
-			args: args{
-				msg:     1,
-				handler: func() {},
-			},
+			cmd:       1,
+			handler:   func() {},
 			wantPanic: "msg must be a struct",
 		},
 		"handler not a func": {
-			args: args{
-				msg:     struct{}{},
-				handler: 1,
-			},
+			cmd:       struct{}{},
+			handler:   1,
 			wantPanic: "handler must be a function",
 		},
-		"handler must have one return value": {
-			args: args{
-				msg:     struct{}{},
-				handler: func(ctx context.Context, msg struct{}) {},
+		"less than two args": {
+			cmd:       struct{}{},
+			handler:   func() error { return nil },
+			wantPanic: "handler must have at least 2 arguments",
+		},
+		"second arg is not a pointer": {
+			cmd:       struct{}{},
+			handler:   func(context.Context, int) error { return nil },
+			wantPanic: "handler's second argument must be a struct pointer",
+		},
+		"second arg is not a struct": {
+			cmd:       struct{}{},
+			handler:   func(context.Context, *int) error { return nil },
+			wantPanic: "handler's second argument must be a struct pointer",
+		},
+		"no return values": {
+			cmd:       struct{}{},
+			handler:   func(ctx context.Context, msg *struct{}) {},
+			wantPanic: "handler must have one return value",
+		},
+		"multiple return values": {
+			cmd: struct{}{},
+			handler: func(ctx context.Context, msg *struct{}) (error, error) {
+				return nil, nil
 			},
 			wantPanic: "handler must have one return value",
+		},
+		"return value not an error": {
+			cmd: struct{}{},
+			handler: func(ctx context.Context, msg *struct{}) int {
+				return 0
+			},
+			wantPanic: "handler's return value must be an error",
+		},
+		"unknown interface": {
+			cmd: struct{}{},
+			handler: func(ctx context.Context, msg *struct{}, s SetIntService) error {
+				return nil
+			},
+			wantPanic: "no providers registered for type van.SetIntService",
+		},
+		"command type mismatch": {
+			cmd: struct{}{},
+			handler: func(ctx context.Context, cmd *Command) error {
+				return nil
+			},
+			wantPanic: "command type mismatch",
 		},
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			defer func() {
-				if r := recover(); r == nil {
-					assertPanic(t, r, tt.wantPanic)
-				}
-			}()
 			bus := New()
-			bus.Handle(tt.args.msg, tt.args.handler)
+			assert.PanicsWithValue(t, tt.wantPanic, func() {
+				bus.HandleCommand(tt.cmd, tt.handler)
+			})
 		})
 	}
+}
+
+func TestHandleCommandFails_AlreadyRegistered(t *testing.T) {
+	bus := New()
+	handler := func(ctx context.Context, cmd *Command) error {
+		return nil
+	}
+	bus.HandleCommand(Command{}, handler)
+	assert.PanicsWithValue(t, "handler already registered for type van.Command", func() {
+		bus.HandleCommand(Command{}, handler)
+	})
+}
+
+func TestInvokeCommand(t *testing.T) {
+	bus := New()
+	var providerExecuted, handlerExecuted int
+	bus.Provide(func() SetIntService {
+		providerExecuted++
+		return &SetIntSevriceImpl{}
+	})
+
+	bus.HandleCommand(Command{}, func(ctx context.Context, cmd *Command, s SetIntService) error {
+		handlerExecuted++
+		return nil
+	})
+
+	ctx := context.Background()
+	for i := 0; i < 5; i++ {
+		err := bus.InvokeCommand(ctx, &Command{})
+		assert.NoError(t, err)
+	}
+
+	assert.Equal(t, providerExecuted, 5)
+	assert.Equal(t, handlerExecuted, 5)
+}
+
+func TestInvokeCommandFails(t *testing.T) {
+	tests := map[string]struct {
+		cmd     interface{}
+		wantErr string
+	}{
+		"cmd is not a pointer": {
+			cmd:     struct{}{},
+			wantErr: "cmd must be a pointer to a struct",
+		},
+		"cmd is not a pointer to struct": {
+			cmd: func() *int {
+				v := 1
+				return &v
+			}(),
+			wantErr: "cmd must be a pointer to a struct",
+		},
+		"unregistered handler": {
+			cmd:     &Command{},
+			wantErr: "no handlers found for type van.Command",
+		},
+	}
+
+	bus := New()
+	ctx := context.Background()
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			err := bus.InvokeCommand(ctx, tt.cmd)
+			assert.Equal(t, tt.wantErr, err.Error())
+		})
+	}
+}
+
+func TestListenEvent(t *testing.T) {
+	bus := New().(*busImpl)
+	handler := func(ctx context.Context, event Event) error {
+		return nil
+	}
+	bus.ListenEvent(Event{}, handler)
+	assert.Len(t, bus.listeners, 1)
+}
+
+func TestListenEvenFails(t *testing.T) {
+	tests := map[string]struct {
+		handler   interface{}
+		wantPanic string
+	}{
+		"not a function": {
+			handler:   struct{}{},
+			wantPanic: "listener must be a function",
+		},
+		"not enough arguments": {
+			handler:   func() error { return nil },
+			wantPanic: "listener must have at least 2 arguments",
+		},
+		"first argument not a context": {
+			handler:   func(ctx struct{}, event Event) error { return nil },
+			wantPanic: "listener's first argument must be the context",
+		},
+		"second argument not a struct": {
+			handler:   func(ctx context.Context, event int) error { return nil },
+			wantPanic: "listener's second argument must be a struct",
+		},
+		"dependency is not an interface": {
+			handler:   func(ctx context.Context, event Event, dep int) error { return nil },
+			wantPanic: "listener's argument 2 must be an interface",
+		},
+		"unknown provider": {
+			handler:   func(ctx context.Context, event Event, dep UnknownService) error { return nil },
+			wantPanic: "no providers registered for type van.UnknownService",
+		},
+	}
+	bus := New()
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.PanicsWithValue(t, tt.wantPanic, func() {
+				bus.ListenEvent(Event{}, tt.handler)
+			})
+		})
+	}
+}
+
+func TestEmitEvent_SingleListener(t *testing.T) {
+	var eventTriggered int
+	listener := func(ctx context.Context, event Event) error {
+		eventTriggered++
+		return nil
+	}
+
+	bus := New()
+	bus.ListenEvent(Event{}, listener)
+	done, errchan := bus.EmitEvent(context.Background(), Event{})
+	<-done
+	assert.Len(t, errchan, 0)
+	assert.Equal(t, eventTriggered, 1)
+}
+
+func TestEmitEvent_MultipleListeners(t *testing.T) {
+	var listenerACalled, listenerBCalled int
+	listenerA := func(ctx context.Context, event Event) error {
+		listenerACalled++
+		return nil
+	}
+	listenerB := func(ctx context.Context, event Event) error {
+		listenerBCalled++
+		return nil
+	}
+
+	bus := New()
+	bus.ListenEvent(Event{}, listenerA, listenerB)
+	done, errchan := bus.EmitEvent(context.Background(), Event{})
+	<-done
+	assert.Len(t, errchan, 0)
+	assert.Equal(t, listenerACalled, 1)
+	assert.Equal(t, listenerBCalled, 1)
+}
+
+func TestEmitEvent_OneListenerFails(t *testing.T) {
+	var eventTriggered int
+	listener := func(ctx context.Context, event Event) error {
+		eventTriggered++
+		return nil
+	}
+
+	listenerErr := errors.New("arbitrary error")
+	badListener := func(ctx context.Context, event Event) error {
+		return listenerErr
+	}
+
+	bus := New()
+	bus.ListenEvent(Event{}, badListener, listener)
+	done, errchan := bus.EmitEvent(context.Background(), Event{Value: 1})
+	<-done
+
+	assert.Equal(t, eventTriggered, 1)
+	assert.Len(t, errchan, 1)
+	assert.Equal(t, listenerErr, <-errchan)
 }
