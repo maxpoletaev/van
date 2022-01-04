baf0f35287e68ff4528027f714344b3f7112118b
Max Poletaev <max.poletaev@gmail.com>
2022-01-04 17:45:03 +0300

    Benchmarks

---
+++ bus_bench_test.go
@@ -0,0 +1,70 @@
+package van
+
+import (
+	"context"
+	"math"
+	"reflect"
+	"testing"
+)
+
+type benchService interface {
+	Run() int
+}
+type serviceA benchService
+type serviceB benchService
+type serviceC benchService
+type serviceD benchService
+type serviceE benchService
+type serviceImpl struct {
+	ret int
+}
+type benchCommand struct {
+	val int
+}
+
+func (s *serviceImpl) Run() int { return s.ret }
+
+func BenchmarkInvokeCommand(b *testing.B) {
+	bus := New()
+	bus.Provide(func() serviceA { return &serviceImpl{ret: 1} })
+	bus.Provide(func(a serviceA) serviceB { return &serviceImpl{ret: 2} })
+	bus.Provide(func(a serviceA, b serviceB) serviceC { return &serviceImpl{ret: 3} })
+	bus.Provide(func(a serviceA, b serviceB, c serviceC) serviceD { return &serviceImpl{ret: 4} })
+	bus.Provide(func(a serviceA, b serviceB, c serviceC, d serviceD) serviceE { return &serviceImpl{ret: 5} })
+	bus.HandleCommand(benchCommand{}, func(ctx context.Context, cmd *benchCommand, a serviceA, b serviceB, c serviceC, d serviceD, e serviceE) error {
+		return nil
+	})
+
+	b.ResetTimer()
+	var err error
+
+	ctx := context.Background()
+	for i := 0; i < b.N; i++ {
+		err = bus.InvokeCommand(ctx, &benchCommand{val: i})
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkFuncCallStatic(b *testing.B) {
+	sqrt := func(v float64) float64 {
+		return math.Sqrt(v)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sqrt(float64(i))
+	}
+}
+
+func BenchmarkFuncCallReflection(b *testing.B) {
+	sqrt := func(v float64) error {
+		math.Sqrt(v)
+		return nil
+	}
+	sqrtV := reflect.ValueOf(sqrt)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sqrtV.Call([]reflect.Value{reflect.ValueOf(float64(i))})
+	}
+}
